d8b2b445c3e7fb5bda452671b0181bf27ae92d64
Jon Allured <jon.allured@gmail.com>
2020-12-29 15:40:29 -0600

    Organize config

--- config.rb
+++ config.rb
@@ -7,13 +7,12 @@ set :markdown, fenced_code_blocks: true
 page 'atom.xml', layout: false
 
 activate :blog do |blog|
+  blog.default_extension = '.md'
+  blog.layout = 'blog'
+  blog.new_article_template = File.expand_path('new_article_template.erb', File.dirname(__FILE__))
   blog.prefix = 'blog'
   blog.sources = '{year}/{month}-{day}-{title}.html'
-  blog.layout = 'blog'
-  # blog.layout = 'default'
-  blog.default_extension = '.md'
   blog.summary_separator = /READ-MORE/
-  blog.new_article_template = File.expand_path('new_article_template.erb', File.dirname(__FILE__))
 end
 
 activate :autoprefixer do |prefix|
